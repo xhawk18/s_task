0aceb849cefb3be3608b68f98041696be36d30d4
X-Hawk <xhawk18@gmail.com>
2020-03-02 16:24:20 +0800

    modify build script.

--- build_linux/cross_build_arm64.sh
+++
@@ -1,9 +0,0 @@
-aarch64-linux-gnu-gcc -O2 -s -static -I../include -Wall \
-	../main.c \
-	../src/s_task.c \
-	../src/s_rbtree.c \
-	../src/s_list.c \
-	../asm/make_arm64_aapcs_elf_gas.S \
-	../asm/jump_arm64_aapcs_elf_gas.S \
-	-o main_arm64
-
