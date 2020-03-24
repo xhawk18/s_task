39b1c02d99d03e3f89ee1bffb445709fe568db84
X-Hawk <xhawk18@gmail.com>
2020-03-24 21:04:46 +0800

    build on mingw and cygwin.

--- build/mingw/build.sh
+++ build/mingw/build.sh
@@ -26,6 +26,37 @@ UV_SOURCE=(
     ../../libuv/src/uv-common.c
     ../../libuv/src/uv-data-getter-setters.c
     ../../libuv/src/version.c
+)
+
+UV_SOURCE_MINGW=(
+    ../../libuv/src/win/async.c
+    ../../libuv/src/win/core.c
+    ../../libuv/src/win/detect-wakeup.c
+    ../../libuv/src/win/dl.c
+    ../../libuv/src/win/error.c
+    ../../libuv/src/win/fs.c
+    ../../libuv/src/win/fs-event.c
+    ../../libuv/src/win/getaddrinfo.c
+    ../../libuv/src/win/getnameinfo.c
+    ../../libuv/src/win/handle.c
+    ../../libuv/src/win/loop-watcher.c
+    ../../libuv/src/win/pipe.c
+    ../../libuv/src/win/thread.c
+    ../../libuv/src/win/poll.c
+    ../../libuv/src/win/process.c
+    ../../libuv/src/win/process-stdio.c
+    ../../libuv/src/win/signal.c
+    ../../libuv/src/win/snprintf.c
+    ../../libuv/src/win/stream.c
+    ../../libuv/src/win/tcp.c
+    ../../libuv/src/win/tty.c
+    ../../libuv/src/win/udp.c
+    ../../libuv/src/win/util.c
+    ../../libuv/src/win/winapi.c
+    ../../libuv/src/win/winsock.c
+)
+
+UV_SOURCE_CYGWIN=(
     ../../libuv/src/unix/async.c
     ../../libuv/src/unix/core.c
     ../../libuv/src/unix/dl.c
@@ -48,6 +79,19 @@ UV_SOURCE=(
     ../../libuv/src/unix/no-fsevents.c
 )
 
+if gcc -dM -E - < /dev/null | grep __MSYS__; then
+    # for cygwin / msys
+    UV_SOURCE=("${UV_SOURCE[@]}" "${UV_SOURCE_CYGWIN[@]}")
+    ST_LIBS=( -lpthread -ldl -lrt )
+elif gcc -dM -E - < /dev/null | grep "__MINGW.*__"; then
+    # for mingw
+    UV_SOURCE=("${UV_SOURCE[@]}" "${UV_SOURCE_MINGW[@]}")
+    ST_LIBS=( -ladvapi32 -liphlpapi -lpsapi -lshell32 -luser32 -luserenv -lws2_32 )
+else
+    echo "neither cygwin nor mingw!" 1>&2
+    exit 1
+fi
+
 "$GCC" -O2 -s -static -I../../include -Wall "${ST_SOURCE[@]}" ../../examples/ex0_task.c -o ex0_task 
 
 "$GCC" -O2 -s -static -I../../include -Wall "${ST_SOURCE[@]}" ../../examples/ex1_event.c -o ex1_event
@@ -55,11 +99,11 @@ UV_SOURCE=(
 "$GCC" -O2 -s -static -I../../include -I../../libuv/include -I../../libuv/src -Wall\
      -DUSE_LIBUV -D_FILE_OFFSET_BITS=64 -D_LARGEFILE_SOURCE -D_GNU_SOURCE \
     "${ST_SOURCE[@]}" "${UV_SOURCE[@]}" \
-    -lpthread -ldl -lrt \
+    "${ST_LIBS[@]}" \
     ../../examples/ex2_libuv.c -o ex2_libuv
 
 "$GCC" -O2 -s -static -I../../include -I../../libuv/include -I../../libuv/src -Wall\
      -DUSE_LIBUV -D_FILE_OFFSET_BITS=64 -D_LARGEFILE_SOURCE -D_GNU_SOURCE \
     "${ST_SOURCE[@]}" "${UV_SOURCE[@]}" \
-    -lpthread -ldl -lrt \
+    "${ST_LIBS[@]}" \
     ../../examples/ex3_http_client.c -o ex3_http_client
