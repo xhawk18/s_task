4e71ec0f7a37df560750283681ee9927f586cfa6
X-Hawk <xhawk18@gmail.com>
2021-02-07 04:09:51 +0800

    add comment.

--- examples/ex5_led_blinking.c
+++ examples/ex5_led_blinking.c
@@ -32,13 +32,13 @@ void sub_task_set_low(__async__, void* arg) {
     while(!g_exit) {
         g_is_low = true;                     /* stop fast blinking */
         LED_SET_LOW();                       /* turn the LED off */
-#ifdef LED2_SET_LOW
-        LED2_SET_HIGH();
+#ifdef LED2_SET_HIGH
+        LED2_SET_HIGH();                     /* turn the 2nd LED on */
 #endif
         s_task_sleep(__await__, 1);          /* wait for 1 second */
         g_is_low = false;                    /* start fast blinking */
 #ifdef LED2_SET_LOW
-        LED2_SET_LOW();
+        LED2_SET_LOW();                      /* turn the 2nd LED off */
 #endif
         s_task_sleep(__await__, 3);          /* wait for 3 seconds */
     }
